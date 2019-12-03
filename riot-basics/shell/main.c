6cd01068656b113f4c561eb3cd4fbea38cfee552
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-12-03 17:17:47 +0100

    riot-basics: add shell notebook and template application

---
+++ riot-basics/shell/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Include the shell header */
+
+/* Implement the shell function callback here */
+
+/* Add the shell command to the list of commands here */
+int main(void)
+{
+    /* Start the shell here */
+
+    return 0;
+}
