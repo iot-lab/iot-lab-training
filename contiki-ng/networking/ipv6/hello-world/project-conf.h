f7dddc0d0edcb5333d84130744e1a34a3f993ff7
Frederic Saint-Marcel <frederic.saint-marcel@inria.fr>
2020-02-10 18:36:04 +0100

    contiki-ng networking: add ipv6 notebook

---
+++ contiki-ng/networking/ipv6/hello-world/project-conf.h
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------*/
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+/*---------------------------------------------------------------------------*/
+
+/* RF Config */
+#ifndef IEEE802154_CONF_PANID
+#define IEEE802154_CONF_PANID                         0xABCD
+#endif
+
+#ifndef IEEE802154_CONF_DEFAULT_CHANNEL
+#define IEEE802154_CONF_DEFAULT_CHANNEL               26
+#endif
+
+/*---------------------------------------------------------------------------*/
+#endif /* PROJECT_CONF_H_ */
