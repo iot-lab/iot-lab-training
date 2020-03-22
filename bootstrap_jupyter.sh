b68b8eac3f174c3bbfcb1e4fbdbb9810c74d5284
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-03-22 17:08:49 +0100

    bootstrap_jupyter.sh: add some updates
    - only source /opt/riot.source if the file is present
    - use bash as default shell

--- bootstrap_jupyter.sh
+++ bootstrap_jupyter.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-source /opt/riot.source && jupyter-lab --no-browser --ip 0.0.0.0 --LabApp.token=iotlab
+RIOT_ENV_FILE=/opt/riot.source
+if [ -f ${RIOT_ENV_FILE} ]; then
+    source ${RIOT_ENV_FILE}
+fi
+
+export SHELL=/bin/bash
+
+jupyter-lab --no-browser --ip 0.0.0.0 --LabApp.token=iotlab
