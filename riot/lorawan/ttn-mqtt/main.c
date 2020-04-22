f8762a852b8491d53bdd0277e6776fd26b579d51
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-04-22 11:37:47 +0200

    riot/lorawan/ttn-mqtt: add notebook for MQTT with TTN

---
+++ riot/lorawan/ttn-mqtt/main.c
@@ -0,0 +1,91 @@
+#include <string.h>
+
+#include "board.h"
+#include "xtimer.h"
+
+#include "net/loramac.h"     /* core loramac definitions */
+#include "semtech_loramac.h" /* package API */
+
+#include "hts221.h"
+#include "hts221_params.h"
+
+
+static semtech_loramac_t loramac;  /* The loramac stack descriptor */
+
+static hts221_t hts221; /* The HTS221 device descriptor */
+
+static const uint8_t deveui[LORAMAC_DEVEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appeui[LORAMAC_APPEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+
+int main(void)
+{
+    /* initialize the HTS221 sensor */
+    if (hts221_init(&hts221, &hts221_params[0]) != HTS221_OK) {
+        puts("Sensor initialization failed");
+        return 1;
+    }
+
+    if (hts221_power_on(&hts221) != HTS221_OK) {
+        puts("Sensor initialization power on failed");
+        return 1;
+    }
+
+    if (hts221_set_rate(&hts221, hts221.p.rate) != HTS221_OK) {
+        puts("Sensor continuous mode setup failed");
+        return 1;
+    }
+
+    /* initialize the loramac stack */
+    semtech_loramac_init(&loramac);
+    
+    /* configure the device parameters */
+    semtech_loramac_set_deveui(&loramac, deveui);
+    semtech_loramac_set_appeui(&loramac, appeui);
+    semtech_loramac_set_appkey(&loramac, appkey);
+
+    /* change datarate to DR5 (SF7/BW125kHz) */
+    semtech_loramac_set_dr(&loramac, 5);
+    
+    /* start the OTAA join procedure */
+    if (semtech_loramac_join(&loramac, LORAMAC_JOIN_OTAA) != SEMTECH_LORAMAC_JOIN_SUCCEEDED) {
+        puts("Join procedure failed");
+        return 1;
+    }
+    puts("Join procedure succeeded");
+
+
+
+    while (1) {
+        /* do some measurements */
+        uint16_t humidity = 0;
+        int16_t temperature = 0;
+        if (hts221_read_humidity(&hts221, &humidity) != HTS221_OK) {
+            puts("Cannot read humidity!");
+        }
+        if (hts221_read_temperature(&hts221, &temperature) != HTS221_OK) {
+            puts("Cannot read temperature!");
+        }
+
+        char message[64];
+        sprintf(message, "H: %d.%d%%, T:%d.%dC",
+                (humidity / 10), (humidity % 10),
+                (temperature / 10), (temperature % 10));
+        printf("Sending message '%s'\n", message);
+
+        /* send the message here */
+        if (semtech_loramac_send(&loramac,
+                                 (uint8_t *)message, strlen(message)) != SEMTECH_LORAMAC_TX_DONE) {
+            printf("Cannot send message '%s'\n", message);
+        }
+        else {
+            printf("Message '%s' sent\n", message);
+        }
+
+        /* wait 30 seconds between each message */
+        xtimer_sleep(30);
+    }
+
+    return 0; /* should never be reached */
+}
