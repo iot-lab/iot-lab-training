cc6792003b18fff5a16764369760dfa9d56f2d56
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-12-21 21:37:01 +0100

    riot/basics/rtc: add notebook for RIOT rtc

---
+++ riot/basics/rtc/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+
+#include "msg.h"
+#include "thread.h"
+
+/* Add rtc peripheral include here */
+
+
+/* Add the RTC alarm callback function here */
+
+
+int main(void)
+{
+    /* Read and print the current time */
+
+
+    do {
+        /* Configure the next alarm */
+
+
+        /* Wait for incoming messages */
+
+
+        /* Read and print the current time */
+
+
+    } while (1);
+
+    return 0;
+}
