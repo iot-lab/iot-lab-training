8f4f413805f094a03402f5503a3e987ad11d15bd
Alexandre Abadie <alexandre.abadie@inria.fr>
2020-12-21 18:04:36 +0100

    riot/basics: add uart notebook

---
+++ riot/basics/uart/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "stdio_uart.h"
+
+#include "msg.h"
+#include "thread.h"
+
+/* Add  uart peripheral include here */
+
+
+/* Declare the message queue here */
+
+
+/* Add the UART callback function here */
+
+
+int main(void)
+{   
+    /* Initialize the STDIO UART */
+
+    
+    /* Wait for incoming messages and print their content */
+
+    
+    return 0;
+}
